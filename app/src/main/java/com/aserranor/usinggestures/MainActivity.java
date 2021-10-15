c9356dea555a1189775474b4d343f925a9c10ffb
AlbertoFBM <aserranor@cifpfbmoll@eu>
2021-10-15 19:36:09 +0200

    Task 2

--- app/src/main/java/com/aserranor/usinggestures/MainActivity.java
+++ app/src/main/java/com/aserranor/usinggestures/MainActivity.java
@@ -5,40 +5,39 @@ import androidx.core.view.MotionEventCompat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String DEBUG_TAG = "Using Gestures";
+    private String DEBUG_TAG = "Using Gestures";
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
+        View view = findViewById(R.id.button);
 
-        int action = MotionEventCompat.getActionMasked(event);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
 
-        switch (action){
-            case (MotionEvent.ACTION_DOWN):
-                Log.d(DEBUG_TAG, "Action was DOWN");
-                return true;
-            case (MotionEvent.ACTION_UP):
-                Log.d(DEBUG_TAG, "Action was UP");
-                return true;
-            case (MotionEvent.ACTION_MOVE):
-                Log.d(DEBUG_TAG, "Action was MOVE");
-                return true;
-            case (MotionEvent.ACTION_CANCEL):
-                Log.d(DEBUG_TAG, "Action was CANCEL");
-                return true;
-            case (MotionEvent.ACTION_OUTSIDE):
-                Log.d(DEBUG_TAG, "Movement occurred outside bounds " +
-                        "of current screen element");
-                return true;
-            default:
-                return super.onTouchEvent(event);
-        }
+                int action = MotionEventCompat.getActionMasked(event);
+
+                switch (action){
+                    case (MotionEvent.ACTION_DOWN):
+                        Log.d(DEBUG_TAG, "View action was DOWN");
+                        return true;
+                    case (MotionEvent.ACTION_UP):
+                        Log.d(DEBUG_TAG,"View action was UP");
+                        return true;
+                    case (MotionEvent.ACTION_MOVE):
+                        Log.d(DEBUG_TAG, "View action was MOVE");
+                        return true;
+                    default:
+                        return MainActivity.super.onTouchEvent(event);
+                }
+            }
+        });
     }
 }
